74520e5f76def605624882d364ca60fd277cf487
escidron <eduardosanchezcidron@gmail.com>
2022-11-12 21:15:15 -0300

    loginpage

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
   return (
     <div className='loginPage'>
         <div className='loginLeft'>
-            <p style={{color:state.color}}>teste color</p>
+            <p style={{color:state.color}}>testes color</p>
             <h1 className='pageTitle'>Login Form Essentials</h1>
             <h4 className='pagesubtitle'>Unlike other types of user interface forms, login forms do not come in all shapes and sizes. They are limited to the number of fields that are used. The reason for such a minimal and capsule approach is trivial: people do not like to fill in forms. You cannot be pushy by asking for too much information. These forms should be simple, familiar, and straight to the point.</h4>
             <div className='midiaIcons'>
